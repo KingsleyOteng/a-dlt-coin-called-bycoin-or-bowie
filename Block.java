8d86caefa7560f057d25d03152d2390a860f63ab
KingsleyOteng <49571112+KingsleyOteng@users.noreply.github.com>
2019-04-23 14:06:40 +0000

    Add files via upload
    We include here a 'miner' method and a hashcheck method.

--- Block.java
+++ Block.java
@@ -16,69 +16,129 @@ import com.google.gson.GsonBuilder;
 
 public class Block {
 
-    public  String newhash;
-    public  String priorHash;
-    private String message;
-    private long timeStamp;
+    public  String newHashSignet;
+    public  String priorHashSignet;
+    private final String message;
+    private final long timeStamp;
+    private int nonce;
     
-    public static ArrayList<Block> blockChain = new ArrayList<Block> ();
+    /**
+     *
+     */
+    public static int miningDepth = 0x6;
     
-    public              Block(String data, String previousHash)
+    public static ArrayList<Block> blockChain = new ArrayList<Block> ();  
+    /**
+     *
+     * @param data
+     * @param previousHash
+     * @throws UnsupportedEncodingException
+     * @throws RuntimeException
+     * @throws NoSuchAlgorithmException
+     */
+    public Block(String data, String previousHash) throws UnsupportedEncodingException , RuntimeException , NoSuchAlgorithmException
         {
-            try
-                {
-                this.message = data;
-                this.priorHash = previousHash;
-                this.timeStamp = new Date().getTime();
-                this.newhash = calculatedhash();
-                }
-            catch (UnsupportedEncodingException | RuntimeException | NoSuchAlgorithmException e)
-                {
             
-                };
-        } 
+                this.message = data;
+                this.priorHashSignet = previousHash;
+                this.timeStamp = new Date().getTime();
+                this.newHashSignet = calculatedhash();
+                
+        };
     
     public final String calculatedhash() throws RuntimeException, NoSuchAlgorithmException, UnsupportedEncodingException 
         {
-            String calculatedhash = signature.applySha256
+            String calculatedhash = SignatureUtil.applySha256
             (
-                priorHash + 
+                priorHashSignet + 
                 Long.toString(timeStamp) + 
+                Integer.toString(nonce) + 
                 message
             );
         
             return calculatedhash;
         };
     
-    public static Boolean isHashCodeCheckValid()
+    public static Boolean isHashCodeCheckValid() throws RuntimeException, NoSuchAlgorithmException, UnsupportedEncodingException
         {
             Block currentBlock;
             Block previousBlock;
+            String hashTarget = new String(new char[miningDepth]).replace('\0','0');
+            
         
+            for(int i=1; i<blockChain.size(); i++)
+                {
+                    currentBlock = blockChain.get(i);
+                    previousBlock = blockChain.get(i-1);
+                    
+                    //reclaculate and compare with the registered hash
+                    if(!currentBlock.newHashSignet.equals(currentBlock.calculatedhash()))
+                    {
+                        System.out.println("Hash check error in current block");
+                        return false;
+                    }
+                    
+                    //compare previous hash and registered hash
+                    if(!previousBlock.newHashSignet.equals(currentBlock.priorHashSignet))
+                    {
+                        System.out.println("Hash check error in previous block");
+                        return false;
+                    }
+                }
+            
             return true;
-        }
+        };
     
-    
+    public void miner(int miningDepth) throws RuntimeException, NoSuchAlgorithmException, UnsupportedEncodingException
+        {
+            String mine = new String(new char[miningDepth]).replace('\0', '0');
+            while (!newHashSignet.substring(0, miningDepth).equals(mine))
+                {
+                        nonce ++;
+                        newHashSignet = calculatedhash();
+                }
+            
+            System.out.println("New Block [" + newHashSignet + "] Succesfully Mined!!! : ");
+        };
     
     /**
      * @param args the command line arguments
      * reference medium.com discussions
+     * @throws java.io.UnsupportedEncodingException
+     * @throws java.security.NoSuchAlgorithmException
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) throws UnsupportedEncodingException, RuntimeException, NoSuchAlgorithmException 
         {
+        
+        blockChain.add(new Block("They call me block", "0"));
+        System.out.println("Trying to 'mine' block 1... ");
+        blockChain.get(0).miner(miningDepth);
+        
+        blockChain.add(new Block("They call me block", blockChain.get(blockChain.size()-1).newHashSignet));
+        System.out.println("Trying to 'mine' block 2... ");
+        blockChain.get(1).miner(miningDepth);
+        
+        blockChain.add(new Block("They call me block", blockChain.get(blockChain.size()-1).newHashSignet));
+        System.out.println("Trying to 'mine' block 3... ");
+        blockChain.get(2).miner(miningDepth);
+        
+        System.out.println("\nBlockchain is Valid: " + isHashCodeCheckValid());
+        
+        String blockchainJsond;
+        blockchainJsond = new GsonBuilder().setPrettyPrinting().create().toJson(blockChain);
+        System.out.println("\nThe block chain:");
+        System.out.println(blockchainJsond);
+        
         // TODO code application logic here
         //Block baseBlock = new Block("This is the first block","0");
         //System.out.println("SHA256 hash for block 1 : " + baseBlock.hashCode());
-        
         //Block secondBlock = new Block("This is the second block","0");
         //System.out.println("SHA256 hash for block 2 : " + secondBlock.hashCode());
-        
         //Block thirdBlock = new Block("This is the third block","0");
         //System.out.println("SHA256 hash for block 3 : " + thirdBlock.hashCode());
-        
         blockChain.add(new Block("This is the first block","0"));
-        blockChain.add(new Block("This is the second block", blockChain.get(blockChain.size()-1).newhash));
-        blockChain.add(new Block("This is the third block", blockChain.get(blockChain.size()-1).newhash));
+        blockChain.add(new Block("This is the second block", blockChain.get(blockChain.size()-1).newHashSignet));
+        blockChain.add(new Block("This is the third block", blockChain.get(blockChain.size()-1).newHashSignet));
         
         String blockChainJsond = new GsonBuilder().setPrettyPrinting().create().toJson(blockChain);
         System.out.println(blockChainJsond);
---
+++ SignatureUtil.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bydlt;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author user
+ */
+public class SignatureUtil {
+    
+        public static String applySha256(String message) throws RuntimeException, NoSuchAlgorithmException, UnsupportedEncodingException
+        {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(message.getBytes("UTF-8"));
+            StringBuilder hexString =new StringBuilder();
+            
+            for (int i =0; i < hash.length; i++)
+            {
+                String hex = 
+                        Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == i) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+}
